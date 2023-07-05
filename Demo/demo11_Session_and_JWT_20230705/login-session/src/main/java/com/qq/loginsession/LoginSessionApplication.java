790ea8c505ab99a57b903fa53b08d5b7c7ef243f
xiebinbin <513614809@qq.com>
2023-07-05 12:51:49 +0800

    【项目实践】一文带你搞定Session和JWT

---
+++ Demo/demo11_Session_and_JWT_20230705/login-session/src/main/java/com/qq/loginsession/LoginSessionApplication.java
@@ -0,0 +1,13 @@
+package com.qq.loginsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSessionApplication.class, args);
+    }
+
+}
